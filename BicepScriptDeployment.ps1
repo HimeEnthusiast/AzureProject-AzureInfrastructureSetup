f1a3835614295ed3f8fb9ad62fddd0eda4e65676
HimeEnthusiast <piantonibella@gmail.com>
2024-12-12 03:08:04 +0000

    Firewall rule

--- BicepScriptDeployment.ps1
+++ BicepScriptDeployment.ps1
@@ -1,27 +1,29 @@
 $groupName = "RG_AzureBicepApplicationDeployment"
 $location = "CanadaCentral"
 $adminDbPass = "MeowMeowMeow123!!"
+$serverName = "sql1357924680.database.windows.net"
 
-Write-Output Set account to BellaFirstSubscription
-az account set --subscription "BellaFirstSubscription"
+# Write-Output Set account to BellaFirstSubscription
+# az account set --subscription "BellaFirstSubscription"
 
-Write-Output Create Resource Group
-az deployment sub create `
---location $location `
---template-file .\ResourceGroupCreation.bicep
+# Write-Output Create Resource Group
+# az deployment sub create `
+# --location $location `
+# --template-file .\ResourceGroupCreation.bicep
 
-Write-Output Create virtual network
-az deployment group create `
---resource-group $groupName `
---template-file .\VirtualNetworkCreation.bicep
+# Write-Output Create virtual network
+# az deployment group create `
+# --resource-group $groupName `
+# --template-file .\VirtualNetworkCreation.bicep
 
-Write-Output Create SQL Server and Database
-az deployment group create `
---resource-group $groupName `
---parameters administratorLoginPassword=$adminDbPass `
---template-file .\SqlDatabaseCreation.bicep
+# Write-Output Create SQL Server and Database
+# az deployment group create `
+# --resource-group $groupName `
+# --parameters administratorLoginPassword=$adminDbPass `
+# --template-file .\SqlDatabaseCreation.bicep
 
 # Firewall #
+Write-Output Create firewall rule
 az sql server firewall-rule create `
 --name AllowAll `
 --resource-group $groupName `
@@ -29,19 +31,19 @@ az sql server firewall-rule create `
 --start-ip-address 0.0.0.0 `
 --end-ip-address 255.255.255.255
 
-Write-Output Create private endpoint for SQL server
-az deployment group create `
---resource-group $groupName `
---parameters privateEndpointIP='10.0.0.1' `
---template-file .\SqlPrivateEndpointCreation.bicep
+# Write-Output Create private endpoint for SQL server
+# az deployment group create `
+# --resource-group $groupName `
+# --parameters privateEndpointIP='10.0.0.1' `
+# --template-file .\SqlPrivateEndpointCreation.bicep
 
-Write-Output Create app service for web app deployment
-az deployment group create `
---resource-group $groupName `
---template-file .\CreateAppService.bicep
+# Write-Output Create app service for web app deployment
+# az deployment group create `
+# --resource-group $groupName `
+# --template-file .\CreateAppService.bicep
 
-Write-Output Create private endpoint for app service
-az deployment group create `
---resource-group $groupName `
---parameters privateEndpointIP='10.0.1.1' `
---template-file .\AppPrivateEndpointCreation.bicep
+# Write-Output Create private endpoint for app service
+# az deployment group create `
+# --resource-group $groupName `
+# --parameters privateEndpointIP='10.0.1.1' `
+# --template-file .\AppPrivateEndpointCreation.bicep
